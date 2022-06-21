9460a9d962b9e77f0a137084dcf449189dad3941
Alexander Entinger <cto@lxrobotics.com>
2022-06-21 09:46:41 +0200

    Building minimal executable.

---
+++ src/main.cpp
@@ -0,0 +1,21 @@
+/**
+ * Copyright (c) 2022 LXRobotics GmbH.
+ * Author: Alexander Entinger <alexander.entinger@lxrobotics.com>
+ * Contributors: https://github.com/107-systems/l3xz_ctrl/graphs/contributors.
+ */
+
+/**************************************************************************************
+ * INCLUDE
+ **************************************************************************************/
+
+#include <rclcpp/rclcpp.hpp>
+
+/**************************************************************************************
+ * MAIN
+ **************************************************************************************/
+
+int main(int argc, char **argv)
+{
+  rclcpp::init(argc, argv);
+  return EXIT_SUCCESS;
+}
