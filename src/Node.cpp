61c1919707971ed005faaec145d0d818753468b4
Alexander Entinger <cto@lxrobotics.com>
2022-06-23 07:02:51 +0200

    Write output of gait controller to output message.

--- src/Node.cpp
+++ src/Node.cpp
@@ -62,8 +62,34 @@ void Node::onCtrlLoopTimerEvent()
   _head_ctrl_output = _head_ctrl.update(_head_ctrl_input, _head_ctrl_output);
 
   l3xz_ctrl::msg::Output msg;
+
   msg.head_target.pan_angle_deg  = _head_ctrl_output.pan_angle ();
   msg.head_target.tilt_angle_deg = _head_ctrl_output.tilt_angle();
+
+  msg.left_front.coxa_angle_deg    = _gait_ctrl_input.get_angle_deg(Leg::LeftFront,   Joint::Coxa);
+  msg.left_front.femur_angle_deg   = _gait_ctrl_input.get_angle_deg(Leg::LeftFront,   Joint::Femur);
+  msg.left_front.tibia_angle_deg   = _gait_ctrl_input.get_angle_deg(Leg::LeftFront,   Joint::Tibia);
+
+  msg.left_middle.coxa_angle_deg   = _gait_ctrl_input.get_angle_deg(Leg::LeftMiddle,  Joint::Coxa);
+  msg.left_middle.femur_angle_deg  = _gait_ctrl_input.get_angle_deg(Leg::LeftMiddle,  Joint::Femur);
+  msg.left_middle.tibia_angle_deg  = _gait_ctrl_input.get_angle_deg(Leg::LeftMiddle,  Joint::Tibia);
+
+  msg.left_back.coxa_angle_deg     = _gait_ctrl_input.get_angle_deg(Leg::LeftBack,    Joint::Coxa);
+  msg.left_back.femur_angle_deg    = _gait_ctrl_input.get_angle_deg(Leg::LeftBack,    Joint::Femur);
+  msg.left_back.tibia_angle_deg    = _gait_ctrl_input.get_angle_deg(Leg::LeftBack,    Joint::Tibia);
+
+  msg.right_front.coxa_angle_deg   = _gait_ctrl_input.get_angle_deg(Leg::RightFront,  Joint::Coxa);
+  msg.right_front.femur_angle_deg  = _gait_ctrl_input.get_angle_deg(Leg::RightFront,  Joint::Femur);
+  msg.right_front.tibia_angle_deg  = _gait_ctrl_input.get_angle_deg(Leg::RightFront,  Joint::Tibia);
+
+  msg.right_middle.coxa_angle_deg  = _gait_ctrl_input.get_angle_deg(Leg::RightMiddle, Joint::Coxa);
+  msg.right_middle.femur_angle_deg = _gait_ctrl_input.get_angle_deg(Leg::RightMiddle, Joint::Femur);
+  msg.right_middle.tibia_angle_deg = _gait_ctrl_input.get_angle_deg(Leg::RightMiddle, Joint::Tibia);
+
+  msg.right_back.coxa_angle_deg    = _gait_ctrl_input.get_angle_deg(Leg::RightBack,   Joint::Coxa);
+  msg.right_back.femur_angle_deg   = _gait_ctrl_input.get_angle_deg(Leg::RightBack,   Joint::Femur);
+  msg.right_back.tibia_angle_deg   = _gait_ctrl_input.get_angle_deg(Leg::RightBack,   Joint::Tibia);
+
   _output_pub->publish(msg);
 }
 
