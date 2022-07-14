d9015c662d8c7ae00c25c04e6111b038664eb0bc
Alexander Entinger <cto@lxrobotics.com>
2022-07-14 10:28:26 +0200

    Provide more detailed error information in case of failure.

--- src/main.cpp
+++ src/main.cpp
@@ -16,10 +16,26 @@
  * MAIN
  **************************************************************************************/
 
-int main(int argc, char **argv)
+int main(int argc, char * argv[])
 {
   rclcpp::init(argc, argv);
-  rclcpp::spin(std::make_shared<l3xz::ControlNode>());
-  rclcpp::shutdown();
-  return EXIT_SUCCESS;
+
+  auto node = std::make_shared<l3xz::ControlNode>();
+
+  try
+  {
+    rclcpp::spin(node);
+    rclcpp::shutdown();
+    return EXIT_SUCCESS;
+  }
+  catch (std::runtime_error const & err)
+  {
+    RCLCPP_ERROR(rclcpp::get_logger(node->get_name()), "Exception (std::runtime_error) caught: %s\nTerminating ...", err.what());
+    return EXIT_FAILURE;
+  }
+  catch (...)
+  {
+    RCLCPP_ERROR(rclcpp::get_logger(node->get_name()), "Unhandled exception caught.\nTerminating ...");
+    return EXIT_FAILURE;
+  }
 }
