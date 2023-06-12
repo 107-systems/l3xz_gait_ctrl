53ebc73dff69c92d1a3737a3b5d22e7a466fc2cd
Alexander Entinger <consulting@lxrobotics.com>
2023-06-12 12:26:46 +0200

    Add dedicated sub-state for performing position trajectory control.

---
+++ include/l3xz_gait_ctrl/gait/state/PositionTrajectory.h
@@ -0,0 +1,46 @@
+/**
+ * Copyright (c) 2023 LXRobotics GmbH.
+ * Author: Alexander Entinger <alexander.entinger@lxrobotics.com>
+ * Contributors: https://github.com/107-systems/l3xz_gait_ctrl/graphs/contributors.
+ */
+
+#pragma once
+
+/**************************************************************************************
+ * INCLUDES
+ **************************************************************************************/
+
+#include "StateBase.h"
+
+#include <l3xz_gait_ctrl/types/Point3D.h>
+
+/**************************************************************************************
+ * NAMESPACE
+ **************************************************************************************/
+
+namespace l3xz::gait::state
+{
+
+/**************************************************************************************
+ * CLASS DECLARATION
+ **************************************************************************************/
+
+class PositionTrajectory : public StateBase
+{
+public:
+  PositionTrajectory(rclcpp::Logger const logger, rclcpp::Clock::SharedPtr const clock, PointVector const & point_vect);
+  virtual ~PositionTrajectory() { }
+  virtual void onEnter(ControllerInput const & input) override;
+  virtual void onExit() override;
+  virtual std::tuple<StateBase *, ControllerOutput> update(kinematic::Engine const & engine, ControllerInput const & input, ControllerOutput const & prev_output) override;
+
+private:
+  PointVector const _point_vect;
+  PointVector::const_iterator _citer;
+};
+
+/**************************************************************************************
+ * NAMESPACE
+ **************************************************************************************/
+
+} /* l3xz::gait::state */
---
+++ src/gait/state/PositionTrajectory.cpp
@@ -0,0 +1,159 @@
+/**
+ * Copyright (c) 2023 LXRobotics GmbH.
+ * Author: Alexander Entinger <alexander.entinger@lxrobotics.com>
+ * Contributors: https://github.com/107-systems/l3xz_gait_ctrl/graphs/contributors.
+ */
+
+/**************************************************************************************
+ * INCLUDES
+ **************************************************************************************/
+
+#include <l3xz_gait_ctrl/gait/state/PositionTrajectory.h>
+
+#include <l3xz_gait_ctrl/gait/state/Sitting.h>
+
+#include <l3xz_gait_ctrl/const/LegList.h>
+
+/**************************************************************************************
+ * NAMESPACE
+ **************************************************************************************/
+
+namespace l3xz::gait::state
+{
+
+/**************************************************************************************
+ * CTOR/DTOR
+ **************************************************************************************/
+
+PositionTrajectory::PositionTrajectory(rclcpp::Logger const logger, rclcpp::Clock::SharedPtr const clock, PointVector const & point_vect)
+: StateBase(logger, clock)
+, _point_vect{point_vect}
+, _citer{_point_vect.cbegin()}
+{
+
+}
+
+/**************************************************************************************
+ * PUBLIC MEMBER FUNCTIONS
+ **************************************************************************************/
+
+void PositionTrajectory::onEnter(ControllerInput const & /* input */)
+{
+  RCLCPP_INFO(_logger, "PositionTrajectory ENTER");
+}
+
+void PositionTrajectory::onExit()
+{
+  RCLCPP_INFO(_logger, "PositionTrajectory EXIT");
+}
+
+std::tuple<StateBase *, ControllerOutput> PositionTrajectory::update(kinematic::Engine const & engine, ControllerInput const & input, ControllerOutput const & prev_output)
+{
+  ControllerOutput next_output = prev_output;
+
+  bool all_target_positions_reached = true;
+  std::stringstream target_position_not_reached_list;
+
+  for (auto leg : LEG_LIST)
+  {
+    double const coxa_deg_actual  = input.get_angle_deg(leg, Joint::Coxa );
+    double const femur_deg_actual = input.get_angle_deg(leg, Joint::Femur);
+    double const tibia_deg_actual = input.get_angle_deg(leg, Joint::Tibia);
+
+    /* Calculate required target angles for desired
+     * target position and set the output actuators.
+     */
+    auto [target_x, target_y, target_z] = *_citer;
+
+    kinematic::IK_Input const ik_input(target_x,
+                                       target_y,
+                                       target_z,
+                                       coxa_deg_actual,
+                                       femur_deg_actual,
+                                       tibia_deg_actual);
+    auto const ik_output = engine.ik_solve(ik_input);
+
+    if (!ik_output.has_value())
+    {
+      RCLCPP_ERROR(_logger,
+                   "engine.ik_solve failed for (%0.2f, %0.2f, %0.2f / %0.2f, %0.2f, %0.2f)",
+                   target_x,
+                   target_y,
+                   target_z,
+                   coxa_deg_actual,
+                   femur_deg_actual,
+                   tibia_deg_actual);
+      return {this, next_output};
+    }
+
+    /* Set output to the angle actuators. */
+    next_output.set_angle_deg(leg, Joint::Coxa,  ik_output.value().coxa_angle_deg ());
+    next_output.set_angle_deg(leg, Joint::Femur, ik_output.value().femur_angle_deg());
+    next_output.set_angle_deg(leg, Joint::Tibia, ik_output.value().tibia_angle_deg());
+
+    /* Check if target position has been reached: First calculate
+     * the Euclidean distance between the actual and the target position.
+     */
+    kinematic::FK_Input const fk_input(coxa_deg_actual, femur_deg_actual, tibia_deg_actual);
+    auto const fk_output = engine.fk_solve(fk_input);
+
+    if (!fk_output.has_value())
+    {
+      RCLCPP_ERROR(_logger,
+                   "engine.fk_solve failed for (%0.2f, %0.2f, %0.2f)",
+                   coxa_deg_actual,
+                   femur_deg_actual,
+                   tibia_deg_actual);
+      return {this, next_output};
+    }
+
+    float const target_err = sqrt(
+      pow((fk_output.value().tibia_tip_x() - target_x), 2.0f) +
+      pow((fk_output.value().tibia_tip_y() - target_y), 2.0f) +
+      pow((fk_output.value().tibia_tip_z() - target_z), 2.0f)
+    );
+
+    if (target_err > 20.0f)
+    {
+      RCLCPP_INFO_THROTTLE(_logger,
+                           *_clock,
+                           1000,
+                           "target = (%0.2f, %0.2f, %0.2f), fk_output = (%0.2f, %0.2f, %0.2f), target_err = %0.2f",
+                           target_x,
+                           target_y,
+                           target_z,
+                           fk_output.value().tibia_tip_x(),
+                           fk_output.value().tibia_tip_y(),
+                           fk_output.value().tibia_tip_z(),
+                           target_err);
+
+      all_target_positions_reached = false;
+      target_position_not_reached_list << LegToStr(leg) << " ";
+    }
+  }
+
+  if (!all_target_positions_reached)
+  {
+    RCLCPP_INFO_THROTTLE(_logger,
+                         *_clock,
+                         1000,
+                         "target position not reached for [ %s]",
+                         target_position_not_reached_list.str().c_str());
+
+    return std::tuple(this, next_output);
+  }
+
+  RCLCPP_INFO(_logger, "transitioning to next step on sitting down trajectory.");
+  _citer++;
+
+  if (_citer != _point_vect.cend())
+    return std::tuple(this, next_output);
+
+  return std::tuple(new Sitting(_logger, _clock), next_output);
+}
+
+/**************************************************************************************
+ * NAMESPACE
+ **************************************************************************************/
+
+} /* l3xz::gait::state */
